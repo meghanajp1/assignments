1fa2d785abc424f5734304699203d8ab2a177521
meghanajp1 <160100482+meghanajp1@users.noreply.github.com>
2024-03-05 17:21:41 +0530

    Add files via upload

---
+++ Spiltfun.java
@@ -0,0 +1,30 @@
+package assignment;
+
+import java.util.Arrays;
+
+public class Spiltfun {
+
+	
+		public static void main(String[] args) {
+			String name  ="meghana chethan belur srinivasu";
+			String[] name1 = name.split(" ");
+			System.out.println(Arrays.toString(name1));
+
+			for(int i =name1.length-1;i>=0;i--)
+				
+			
+			{
+				System.out.print(name1[i]+" ");
+				
+			}
+			
+			
+			
+
+
+			
+			}
+		
+	}
+
+
